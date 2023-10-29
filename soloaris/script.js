e9b7f3bde63c6acb8ca7f73796616093a88077fa
JacobWHM <146091832+JacobWHM@users.noreply.github.com>
2023-10-29 20:24:52 +0100

    solaris

---
+++ soloaris/script.js
@@ -0,0 +1,15 @@
+const url ="https://majazocom.github.io/Data/solaris.json";
+
+
+const fetchbodies = async () => {
+    try {
+      const response = await fetch("https://majazocom.github.io/Data/solaris.json");
+      if (!response.ok) {
+        throw new Error("Failed to fetch bodies");
+      }
+      
+    } catch (error) {
+      console.error("Error fetching bodies:", error);
+    }
+  };
+  fetchbodies();
