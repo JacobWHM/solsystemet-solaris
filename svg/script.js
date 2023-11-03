040d3927d072a35ef51819b16cca0b91f8490f33
JacobWHM <146091832+JacobWHM@users.noreply.github.com>
2023-11-03 09:01:27 +0100

    solaris

--- svg/script.js
+++ svg/script.js
@@ -1,18 +1,51 @@
-const url ="https://majazocom.github.io/Data/solaris.json";
+
+//get data
 
 
-const fetchbodies = async () => {
-    try {
-      const response = await fetch("https://majazocom.github.io/Data/solaris.json");
-      if (!response.ok) {
-        throw new Error("Failed to fetch bodies");
-      }
-      
-    } catch (error) {
-      console.error("Error fetching bodies:", error);
+const fetchData = async () => {
+  try {
+    const response = await fetch(
+      "https://majazocom.github.io/Data/solaris.json"
+    );
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
     }
-    const data = await result.json()
-      return data
-    
-  };
-  fetchbodies();
+
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("There was a problem with the fetch operation:", error);
+  }
+};
+
+fetchData();
+
+
+// get the infromation about the planets
+// step 1 get only the data
+function fetchPlanets() {
+  fetch("https://majazocom.github.io/Data/solaris.json")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      // step 2 send the data to the funtion the display the button and the list
+      
+    displayPlanets(data); 
+    });
+}
+function displayPlanets(planets) {
+  const planetList = document.getElementById("planetList");
+
+  planets.forEach((planet) => {
+    const listItem = document.createElement("li");
+    const button = document.createElement("button");
+    button.textContent = "Get Info";
+    button.addEventListener("click", () => displayPlanets(planet));
+
+    listItem.textContent = planet.name;
+    listItem.appendChild(button);
+    planetList.appendChild(listItem);
+  });
+}
+
